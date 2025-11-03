c07e921ba9de2824f295b9fb48a01e56c109571a
ailyncoronaa <ailyncoronaa@gmail.com>
2025-11-02 21:16:21 -0500

    added requirements of assignement

--- hangman.js
+++ hangman.js
@@ -4,17 +4,25 @@ var MAX_GUESSES = 6;
 var word="";
 var guesses = "";
 var guess_count = MAX_GUESSES;
+var gameOver = false;
 
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
     guess_count = MAX_GUESSES;
+
+    document.getElementById("clue").innerHTML = "";
+    document.getElementById("guesses").innerHTML = "Guessed Letters: ";
+    document.getElementById("hangmanImage").src = "images/hangman6.gif";
     updatePage();
     }
+
 function guessLetter() {
+    if (gameOver || word === "") return;
     var input = document.getElementById("guess");
     var letter = input.value;
+    if (letter === "" || guesses.indexOf(letter) >= 0) return;
     if (word.indexOf(letter) < 0) {
         guess_count--;
     }
@@ -24,6 +32,8 @@ function guessLetter() {
 }
 function updatePage() {
     var clueString = "";
+    var allGuessed = true; 
+
     for (var i = 0; i < word.length; i++) {
         var currentLetter = word.charAt(i);
         if (guesses.indexOf(currentLetter) >= 0) {//You guessed it
@@ -31,16 +41,20 @@ function updatePage() {
         }
         else
             clueString += "_ ";
+            allGuessed = false;
     }
-
-    var clue = document.getElementById("clue");
-    clue.innerHTML = clueString;
-
-    
-    var guessArea = document.getElementById("guesses");
-    guessArea.innerHTML = "Guessed Letters: " + guesses;
-
+}
+document.getElementById("clue").innerHTML = clueString;
+var guessArea = document.getElementById("guesses");
+guessArea.innerHTML = "Guessed Letters" + guesses;
 
 var image = document.getElementById("hangmanImage");
-image.src = "images/hangman6.gif"
+image.src = "images/hangman" + guess_count + ".gif";
+
+if (allGuessed) {
+    guessArea.innerHTML += "<br> You Win!";
+    gameOver = true;
+} else if  (guess_count <= 0) {
+    guessArea.innerHTML += "<br>You LOSE! The word was: " + word;
+    gameOver = true;
 }
