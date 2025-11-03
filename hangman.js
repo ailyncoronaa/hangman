8b1449ec0e0654e20db2a5e1c787899f9cc9b537
ailyncoronaa <ailyncoronaa@gmail.com>
2025-11-02 19:19:43 -0500

    fixing html and js

--- hangman.js
+++ hangman.js
@@ -1,18 +1,26 @@
 var POSSIBLE_WORDS = ["obdurate", "verisimilitude",
     "defenestrate", "obsequious", "dissonant", "today", "idempotent"];
+var MAX_GUESSES = 6;
 var word="";
 var guesses = "";
+var guess_count = MAX_GUESSES;
+
 function newGame(){
     var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
+    guess_count = MAX_GUESSES;
     updatePage();
     }
 function guessLetter() {
     var input = document.getElementById("guess");
     var letter = input.value;
+    if (word.indexOf(letter) < 0) {
+        guess_count--;
+    }
     guesses += letter;
     updatePage();
+    input.value = "";
 }
 function updatePage() {
     var clueString = "";
@@ -24,9 +32,15 @@ function updatePage() {
         else
             clueString += "_ ";
     }
+
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
 
+    
     var guessArea = document.getElementById("guesses");
     guessArea.innerHTML = "Guessed Letters: " + guesses;
+
+
+var image = document.getElementById("hangmanImage");
+image.src = "images/hangman6.gif"
 }
